09b601cf7fcfec872df4f16c4ce82191f82dbe45
Jabaca <javi.banos10@gmail.com>
2020-05-20 13:30:27 +0200

    init commit

---
+++ src/probargit/ProbarGit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probargit;
+
+/**
+ *
+ * @author Jabaca10
+ */
+public class ProbarGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    coche car=new coche();
+    car.encender();
+    car.gasolina();
+    car.apagar();
+        
+    }
+}
